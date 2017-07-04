7fbdf0c1fee916e7c048b2061204b27be8fe5706
duoduozh <root@myCentOS73.lheakmkh0hfexpmn3jtct01yog.jx.internal.cloudapp.net>
2017-07-04 15:36:37 +0000

    add insert and query data example

--- tst/mgo.go
+++ tst/mgo.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "gopkg.in/mgo.v2"
     "gopkg.in/mgo.v2/bson"
+    "time"
 )
 
 type Training struct {
@@ -39,8 +40,18 @@ type Module struct {
     Grade           int                 `bson:"grade"`
 }
 
+type Person struct {
+    ID              bson.ObjectId       `bson:"_id,omitempty"`
+    Name            string
+    Phone           string
+    Timestamp       time.Time
+}
+
+var ( 
+    IsDrop = true
+)
 func main(){
-    session, err := mgo.Dial("127.0.0.1:27017")
+    session, err := mgo.Dial("127.0.0.1")
     
     if err != nil {
         panic(err)
@@ -49,5 +60,56 @@ func main(){
     }
 
     defer session.Close()
+
+    session.SetMode(mgo.Monotonic, true)
+
+    // Drop DB
+    if IsDrop {
+        err = session.DB("test").DropDatabase()
+        if err != nil {
+            panic(err)
+        }
+    } 
+
+    // Collection People
+    c := session.DB("test").C("people")
+
+    // Index
+    index := mgo.Index{
+        Key: []string{"name", "phone"},
+        Unique: true,
+        DropDups: true,
+        Background: true,
+        Sparse: true,
+    }
+
+    err = c.EnsureIndex(index)
+    if err != nil {
+        panic(err)
+    }
+
+    // Insert Datas
+    err = c.Insert(&Person{Name: "Ale", Phone: "+55 53 1234 4321", Timestamp: time.Now()},
+                    &Person{Name: "Cla", Phone: "+66 33 1234 5678", Timestamp: time.Now()})
+
+    if err != nil {
+        panic(err)
+    }
+
+    // Query One
+    result := Person{}
+    err = c.Find(bson.M{"name": "Ale"}).Select(bson.M{"phone": 0}).One(&result)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("Result Person", result)
+
+    // Query All
+    var resultPersonList []Person
+    err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&resultPersonList)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("Result Person List", resultPersonList)
 
 }
