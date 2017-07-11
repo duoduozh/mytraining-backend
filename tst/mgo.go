e87da5bf798b51b35115cfa14b1c22fbb5b11326
carson8349 <myzn007@hotmail.com>
2017-07-11 13:38:00 +0800

    add more demo code about embedded documents

--- tst/mgo.go
+++ tst/mgo.go
@@ -1,115 +1,135 @@
 package main
 
 import (
-    "fmt"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "time"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"models"
+	"time"
 )
 
 type Training struct {
-    Id              bson.ObjectId       `bson:"_id,omitempty"` 
-    Name            string              `bson:"name"`
-    Overview        string              `bson:"overview"`
-    BasicInfo       string              `bson:"basic_info"`
-    Commitment      string              `bson:"commitment"`
-    HowToPass       string              `bson:"how_to_pass"`
-    AverageRating   int                 `bson:"average_rating"`
-    Icon            string              `bson:"icon"`
-    Specification   string              `bson:"specification_info"`
-    Forum           string              `bson:"forum"`
-    Language        []string            `bson:"language"`
+	Id            bson.ObjectId `bson:"_id,omitempty"`
+	Name          string        `bson:"name"`
+	Overview      string        `bson:"overview"`
+	BasicInfo     string        `bson:"basic_info"`
+	Commitment    string        `bson:"commitment"`
+	HowToPass     string        `bson:"how_to_pass"`
+	AverageRating int           `bson:"average_rating"`
+	Icon          string        `bson:"icon"`
+	Specification string        `bson:"specification_info"`
+	Forum         string        `bson:"forum"`
+	Language      []string      `bson:"language"`
 }
 
 type Lecture struct {
-    Id              bson.ObjectId       `bson:"_id,omitempty"`
-    Name            string              `bson:"name"`
-    Organization    string              `bson:"organization"`
+	Id           bson.ObjectId `bson:"_id,omitempty"`
+	Name         string        `bson:"name"`
+	Organization string        `bson:"organization"`
 }
 
 type Module struct {
-    Id              bson.ObjectId       `bson:"_id,omitempty"`
-    Week            int                 `bson:"week"`
-    Module          string              `bson:"module"`
-    Title           string              `bson:"title"`
-    Description     string              `bson:"description"`
-    Video           int                 `bson:"video"`
-    Reading         int                 `bson:"reading"`
-    Practice        int                 `bson:"practice"`
-    Duration        int                 `bson:"duration"`
-    Grade           int                 `bson:"grade"`
+	Id          bson.ObjectId `bson:"_id,omitempty"`
+	Week        int           `bson:"week"`
+	Module      string        `bson:"module"`
+	Title       string        `bson:"title"`
+	Description string        `bson:"description"`
+	Video       int           `bson:"video"`
+	Reading     int           `bson:"reading"`
+	Practice    int           `bson:"practice"`
+	Duration    int           `bson:"duration"`
+	Grade       int           `bson:"grade"`
 }
 
-type Person struct {
-    ID              bson.ObjectId       `bson:"_id,omitempty"`
-    Name            string
-    Phone           string
-    Timestamp       time.Time
-}
-
-var ( 
-    IsDrop = true
+var (
+	IsDrop = true
 )
-func main(){
-    session, err := mgo.Dial("127.0.0.1")
-    
-    if err != nil {
-        panic(err)
-    } else {
-        fmt.Printf("connect success\n")
-    }
 
-    defer session.Close()
+func main() {
+	session, err := mgo.Dial("127.0.0.1")
 
-    session.SetMode(mgo.Monotonic, true)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("connect success\n")
+	}
 
-    // Drop DB
-    if IsDrop {
-        err = session.DB("test").DropDatabase()
-        if err != nil {
-            panic(err)
-        }
-    } 
+	defer session.Close()
 
-    // Collection People
-    c := session.DB("test").C("people")
+	session.SetMode(mgo.Monotonic, true)
 
-    // Index
-    index := mgo.Index{
-        Key: []string{"name", "phone"},
-        Unique: true,
-        DropDups: true,
-        Background: true,
-        Sparse: true,
-    }
+	// Drop DB
+	if IsDrop {
+		err = session.DB("test").DropDatabase()
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    err = c.EnsureIndex(index)
-    if err != nil {
-        panic(err)
-    }
+	// Collection People
+	c := session.DB("test").C("people")
 
-    // Insert Datas
-    err = c.Insert(&Person{Name: "Ale", Phone: "+55 53 1234 4321", Timestamp: time.Now()},
-                    &Person{Name: "Cla", Phone: "+66 33 1234 5678", Timestamp: time.Now()})
+	// Index
+	index := mgo.Index{
+		Key:        []string{"name", "phone"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
 
-    if err != nil {
-        panic(err)
-    }
+	err = c.EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
 
-    // Query One
-    result := Person{}
-    err = c.Find(bson.M{"name": "Ale"}).Select(bson.M{"phone": 0}).One(&result)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Result Person", result)
+	person1 := models.Person{
+		Name:      "Ale",
+		Phone:     "+55 53 1234 4321",
+		Timestamp: time.Now(),
+		FAQList: []models.FAQ{
+			models.FAQ{Question: "q1", Answer: "a1"},
+			models.FAQ{Question: "q2", Answer: "a2"},
+		},
+	}
+	// Insert Datas
+	err = c.Insert(&person1,
+		&models.Person{Name: "Cla", Phone: "+66 33 1234 5678", Timestamp: time.Now()})
 
-    // Query All
-    var resultPersonList []Person
-    err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&resultPersonList)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Result Person List", resultPersonList)
+	if err != nil {
+		panic(err)
+	}
+
+	// Query One
+	result := models.Person{}
+	err = c.Find(bson.M{"name": "Ale"}).Select(bson.M{"phone": 0}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Result Person", result)
+
+	// Query All
+	var resultPersonList []models.Person
+	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&resultPersonList)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Result Person List", resultPersonList)
+
+	// Update
+	colQuerier := bson.M{"name": "Ale"}
+	change := bson.M{"$set": bson.M{"phone": "+86 99 8888 7776", "timestamp": time.Now()}}
+	err = c.Update(colQuerier, change)
+	if err != nil {
+		panic(err)
+	}
+
+	// Query All
+	err = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&resultPersonList)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Results All: ", resultPersonList)
 
 }
